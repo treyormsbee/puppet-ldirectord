eebad241d5d2421d5237b31e29da1875a2e68699
treyormsbee <treyormsbee@gmail.com>
2014-12-04 12:58:35 -0600

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,33 +7,27 @@
 # Document parameters here.
 #
 # [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
 #
 # === Variables
 #
 # Here you should define a list of variables that this module would require.
 #
 # [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
 #
 # === Examples
 #
 #  class { 'ldirectord':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#    quiescent => 'no',
+#    connecttimeout => 10
 #  }
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Author Name <treyormsbee@gmail.com>
 #
 # === Copyright
 #
-# Copyright 2014 Your name here, unless otherwise noted.
+# Copyright 2014 Trey Ormsbee, unless otherwise noted.
 #
 class ldirectord(
   $download_location = undef,
